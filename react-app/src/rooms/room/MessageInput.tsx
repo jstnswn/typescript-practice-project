f1a6db3540c5b249f01dacba2346cc26ddf7e799
jstnswn <jstnswn@gmail.com>
2022-07-09 13:02:34 -0700

    test

---
+++ react-app/src/rooms/room/MessageInput.tsx
@@ -0,0 +1,8 @@
+import { FC, ReactElement, useState } from 'react';
+
+const MessageInput: FC = (): ReactElement => {
+    const [textInput, setTextInput] = useState<string>()
+    return <></>
+}
+
+export default MessageInput;
